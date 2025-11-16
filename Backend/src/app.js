5d23af0e782d37d6aed6d54c78a3770b71348a38
Pulkitkumar07 <pulkitchaudhary519@gmail.com>
2025-11-16 21:04:58 +0530

    final deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,27 @@
-const express=require("express");
-const cookieParser=require('cookie-parser')
-const authRoutes=require("./routes/auth.routes")
-const chatRoutes=require('./routes/chat.routes')
-const cors =require('cors')
-/* Using middleware */
-const app=express();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
+const chatRoutes = require('./routes/chat.routes');
+const cors = require('cors');
+const path = require("path");
+
+const app = express();
+
+/* Middleware */
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
+    origin: 'http://localhost:5173',
+    credentials: true
 }));
 
-/* Using Routes */
-app.use('/api/auth',authRoutes)
-app.use('/api/chat',chatRoutes)
+/* Routes */
+app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoutes);
 
-app.get('/',(req,res)=>{
-    res.sendFile(this.path.join(__dirname,'../public/index.html'));
-})  
+/* Serve frontend */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
-module.exports=app;
+module.exports = app;
