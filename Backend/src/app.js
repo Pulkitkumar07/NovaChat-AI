416f946b348e2b8325973b89ffa9a7d923b25705
Pulkitkumar07 <pulkitchaudhary519@gmail.com>
2025-11-19 13:10:12 +0530

    Fix route to correctly match wildcard for serving index.html

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,7 +19,7 @@ app.use('/api/chat',chatRoutes)
 
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
-app.get("*", (req, res) => {
+app.get("*name", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'dist', 'index.html'));
 });
 
